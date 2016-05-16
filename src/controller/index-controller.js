a29ac7e7345e8722d80c62187dcf62880cc44485
dukai <dukai86@gmail.com>
2016-05-16 18:34:07 +0800

    修改弹出层

--- src/controller/index-controller.js
+++ src/controller/index-controller.js
@@ -5,45 +5,61 @@ var conf = require('lib/confrw.js');
 var Vue = require('vue');
 
 Vue.component('modal', {
-    template: loadtp('modal'),
-    props: {
-        show: {
-            type: Boolean,
-            required: true,
-            twoWay: true    
-        }
+  template: loadtp('modal'),
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+      twoWay: true    
     }
+  }
 })
 
 var projectInfo = new Vue({
-    el: '.project-info',
-    data: {
-        projectPath: ''
-    }
+  el: '.project-info',
+  data: {
+    projectPath: ''
+  }
 });
 
 new Vue({
-    el: '.btn-group',
-    data: {
-        showModal: false
-    },
-    methods: {
-        setDir: function(){
-            const dialog = require('electron').remote.dialog;
-            var path = dialog.showOpenDialog({ properties: [ 'openDirectory' ]});
-            if(path === undefined){
-                return;
-            }
-            var configfile = path + require('path').sep + 'config.json';
-            var data = {
-                name: 'om api',
-                author: 'dukai'
-            };
-            require('fs').writeFileSync(configfile, JSON.stringify(data, null, 4));
+  el: '.btn-group',
+  data: {
+    showModal: false
+  },
+  methods: {
+    setDir: function(){
+      const dialog = require('electron').remote.dialog;
+      var path = dialog.showOpenDialog({ properties: [ 'openDirectory' ]});
+      if(path === undefined){
+        return;
+      }
+      var configfile = path + require('path').sep + 'config.json';
+      var data = {
+        name: 'om api',
+        author: 'dukai'
+      };
+      require('fs').writeFileSync(configfile, JSON.stringify(data, null, 4));
 
-            projectInfo.projectPath = path;
-        }
+      projectInfo.projectPath = path;
     }
+  }
 });
 
 require('./tree');
+
+
+new Vue({
+  el: '.dropdown.project',
+  data: {
+    showCreateProject: true,
+    showOpenProject: false,
+  },
+  methods: {
+    saveProject: function(){
+      this.showCreateProject = false;
+    }
+  }
+});
+
+
